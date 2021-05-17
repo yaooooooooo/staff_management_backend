3c32bf2942a51f1d4355c90f4069ffb597fe4ece
chengeng1107 <chengeng1107@163.com>
2021-05-17 22:55:57 +0800

    [geng.chen]#N/A feat:add Swagger for Project.

--- src/main/java/com/accenture/staffmanagement/controller/UserController.java
+++ src/main/java/com/accenture/staffmanagement/controller/UserController.java
@@ -2,6 +2,7 @@ package com.accenture.staffmanagement.controller;
 
 import com.accenture.staffmanagement.entity.User;
 import com.accenture.staffmanagement.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,13 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiOperation("Get User by Eid.")
     @GetMapping(value = "/info/eid/{eid}", produces = "application/json;charset=utf-8")
     public User login(@PathVariable("eid") String eid) {
         return userService.getUserByEid(eid);
     }
 
+    @ApiOperation("Get User by id.")
     @GetMapping(value = "/info/id/{id}", produces = "application/json;charset=utf-8")
     public User getUserInfo(@PathVariable("id") long id) {
         return userService.getUserById(id);
